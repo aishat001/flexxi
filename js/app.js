f8e547058249044b1e9e8f014ff5c8fcf6891483
aishat001 <waheedaishat@gmail.com>
2021-10-10 15:34:02 +0100

    new files

--- js/app.js
+++ js/app.js
@@ -37,20 +37,20 @@ function fn() {
     activateMenu();
 }
 
-      $(document).on('change', '.file-input', function () {
+    // //   $(document).on('change', '.file-input', function () {
 
 
-            var filesCount = $(this)[0].files.length;
+    //         var filesCount = $(this)[0].files.length;
 
-            var textbox = $(this).prev();
+    //         var textbox = $(this).prev();
 
-            if (filesCount === 1) {
-                var fileName = $(this).val().split('\\').pop();
-                textbox.text(fileName);
-            } else {
-                textbox.text(filesCount + ' files selected');
-            }
-        });;
+    //         if (filesCount === 1) {
+    //             var fileName = $(this).val().split('\\').pop();
+    //             textbox.text(fileName);
+    //         } else {
+    //             textbox.text(filesCount + ' files selected');
+    //         }
+    //     });;
 //Menu
 // Toggle menu
 function toggleMenu() {
